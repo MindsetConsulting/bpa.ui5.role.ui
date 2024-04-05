01352dfa5e578163805b238fdcca7f8ef7d07842
Mark Smith <marksmith@mindsetconsulting.com>
2024-04-05 23:24:06 +0000

    Working comboBox

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ UUID: 95e362ab-8256-4a03-b70a-62c51033576f
 
 ![image](https://github.com/MindsetConsulting/bpa.ui5.role.ui/assets/91226856/34cb2cd3-978f-4874-848f-7e41e3781dc7)
 ![image](https://github.com/MindsetConsulting/bpa.ui5.role.ui/assets/91226856/93786f14-443c-431a-b0be-9d5709ab856a)
+
+
+You may also encounter a CORS error when testing locally. In order to by pass the CORS policy. You will need to install a browswer plugin or create a proxy (there are other options as well).
+I used the Google Chrome plugin : https://chromewebstore.google.com/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf
--- workflow-ui-module/webapp/controller/App.controller.js
+++ workflow-ui-module/webapp/controller/App.controller.js
@@ -1,18 +1,49 @@
 sap.ui.define(
-    [
-        "sap/ui/core/mvc/Controller"
-    ],
-    function(BaseController) {
-      "use strict";
-  
-      return BaseController.extend("bparoleassignmentui.workflowuimodule.controller.App", {
-        onInit() {
-          var sServiceUrl = "https://s4hana2022.mindsetconsulting.com:44301/sap/opu/odata/sap/ZB_USER_ASSIGNMENT/";
-          var oModel = new sap.ui.model.odata.v2.ODataModel(sServiceUrl);
-          this.getView().setModel(oModel); 
-      
-        }
-      });
-    }
-  );
-  
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+  ],
+  function (BaseController, JSONModel) {
+    "use strict";
+
+    return BaseController.extend("bparoleassignmentui.workflowuimodule.controller.App", {
+      onInit() {
+
+// will need to remove credentials and store in .env file. Need to figure out correct syntax to get them in controller
+        const odataUrl = "https://s4hana2022.mindsetconsulting.com:44301/sap/opu/odata/sap/ZB_USER_ASSIGNMENT/profilesVH";
+        const username = "BPA_TECH_USE";
+        const password = "Mindset1";
+        var oModel = new JSONModel();
+        this.getView().setModel(oModel); // Set the model with a name
+
+        var that = this;
+        $.ajax({
+          url: odataUrl,
+          type: 'GET',
+          dataType: 'json',
+          beforeSend: function (xhr) {
+            xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password));
+          },
+          success: function (data) {
+            oModel.setData(data);
+            var oComboBox = that.getView().byId("profileDropDown"); // Get your comboBox
+            oComboBox.bindItems({
+              path: "/d/results",
+              template: new sap.ui.core.ListItem({
+                key: "{ProfileName}",
+                text: "{ProfileName}"
+              })
+
+            });
+          },
+
+          error: function (jqXHR, textStatus, errorThrown) {
+            console.error("Error fetching OData:", textStatus, errorThrown);
+          }
+        });
+
+
+      }
+    });
+  }
+);
