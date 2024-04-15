14773fa05695d289ce36417eda7bd163a62e54a5
Jason Inouye <jasoninouye@mindsetconsulting.com>
2024-04-15 21:00:27 +0000

    Message Toast and Comment Box

--- workflow-ui-module/webapp/controller/App.controller.js
+++ workflow-ui-module/webapp/controller/App.controller.js
@@ -1,14 +1,45 @@
 sap.ui.define(
   [
+    "sap/m/MessageToast",
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel"
   ],
-  function (BaseController, JSONModel) {
+  function (MessageToast, BaseController, JSONModel) {
     "use strict";
 
     return BaseController.extend("bparoleassignmentui.workflowuimodule.controller.App", {
       onInit() {
-
+        
+      },
+      handleSelectionChange: function(oEvent) {
+        var changedItem = oEvent.getParameter("changedItem");
+        var isSelected = oEvent.getParameter("selected");
+  
+        var state = "Selected";
+        if (!isSelected) {
+          state = "Deselected";
+        }
+  
+        MessageToast.show("Event 'selectionChange': " + state + " '" + changedItem.getText() + "'", {
+          width: "auto"
+        });
+      },
+      handleSelectionFinish: function(oEvent) {
+        var selectedItems = oEvent.getParameter("selectedItems");
+        var messageText = "Event 'selectionFinished': [";
+  
+        for (var i = 0; i < selectedItems.length; i++) {
+          messageText += "'" + selectedItems[i].getText() + "'";
+          if (i != selectedItems.length - 1) {
+            messageText += ",";
+          }
+        }
+  
+        messageText += "]";
+  
+        MessageToast.show(messageText, {
+          width: "auto"
+        });
       }
     });
   }
